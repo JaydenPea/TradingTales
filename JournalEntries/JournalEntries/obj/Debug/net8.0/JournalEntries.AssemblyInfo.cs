4fea7db68c089a1cbd8c2a7b7d2f335120ef3274
Jayden <jayden.pearce12@gmail.com>
2024-07-20 13:09:39 +0200

    push test

--- JournalEntries/JournalEntries/obj/Debug/net8.0/JournalEntries.AssemblyInfo.cs
+++ JournalEntries/JournalEntries/obj/Debug/net8.0/JournalEntries.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalEntries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57104fa364baec7790c0b90840cedb1f7c4d2169")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalEntries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalEntries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
